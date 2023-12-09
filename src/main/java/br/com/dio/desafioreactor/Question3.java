2bbfd0ac4234871227d805be389ed5426d264988
Gajsilva <devgabrielsj@gmail.com>
2023-12-09 00:25:45 -0300

    Initial commit

---
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -0,0 +1,21 @@
+package br.com.dio.desafioreactor;
+
+import reactor.core.publisher.Mono;
+
+public class Question3 {
+
+    /*
+    Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
+    (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
+     */
+    public Mono<Void> userIsValid(final User user) {
+        return Mono.defer(() -> {
+            if (user != null && user.password() != null && user.password().length() > 8) {
+                return Mono.empty(); // Usuário é válido, Mono<Void> vazio.
+            } else {
+                return Mono.error(new IllegalArgumentException("Usuário inválido"));
+            }
+        });
+    }
+
+}
